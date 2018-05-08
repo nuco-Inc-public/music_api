a6ac931d70cf3a5ed5c8ffd46f85b4a49a585bfc
ishiguro <yuu.ishiguro@gmail.com>
2018-05-08 18:52:30 +0900

    roooooting

--- server.go
+++ server.go
@@ -1,12 +1,40 @@
+// package main
+
+// import (
+//     "net/http"
+// )
+
+// func main() {
+//     http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+// 			w.Header().Add("Access-Control-Allow-Origin", "*")
+// 			//w.Header().Add("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
+// 			w.Write([]byte("Hello"))
+//     })
+//     http.ListenAndServe(":3000", nil)
+// }
+
 package main
 
 import (
-    "net/http"
+	"net/http"
+	// "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-        w.Write([]byte("Hello world"))
-    })
-    http.ListenAndServe(":3000", nil)
+	
+	
+	// r := mux.NewRouter()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Write([]byte("HOME"))
+	})
+
+	http.HandleFunc("/music_list", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Write([]byte("MUSIC_LIST"))
+	})
+	// http.Handle("/", r)
+  http.ListenAndServe(":3000", nil)
+    
 }
