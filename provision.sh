c0f14a1165a5df2d9706768bb2ba76b813cae0ab
ishiguro <yuu.ishiguro@gmail.com>
2018-05-08 12:13:50 +0900

    shell

--- provision.sh
+++ provision.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
+wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
+tar -xzvf go1.10.2.linux-amd64.tar.gz
+sudo mv go /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin' >> .bash_profile
+source ~/.bash_profile
