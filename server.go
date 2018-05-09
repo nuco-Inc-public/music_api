ab913f12be8d1b81e7abd9a2f6cba417f622bdb4
Shogo Sekine <gan.gun.kyosho@gmail.com>
2018-05-09 15:15:33 +0900

    get json from server

--- server.go
+++ server.go
@@ -4,19 +4,49 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
+	"os"
+	"fmt"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Write([]byte("HOME"))
-	})
+	http.Handle("/", BaseHandlerFunc(HomeIndex))
+	http.Handle("/music_list", BaseHandlerFunc(MusicListIndex))
 
-	http.HandleFunc("/music_list", func(w http.ResponseWriter, r *http.Request) {
+	err := http.ListenAndServe(":3000", nil)    
+	if err != nil {
+		fmt.Printf("Failed to connect port %s", err)
+	}
+}
+
+func HomeIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("NOT FOUND"))
+	} else {
+		w.Write([]byte("HOME"))
+	}
+}
+
+func MusicListIndex(w http.ResponseWriter, r *http.Request) {
+	file, e := ioutil.ReadFile("./assets/json/music_list.json")
+	if e != nil {
+		// fmt.Printf("file error: %v\n", e)
+		os.Exit(1)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(file)
+}
+
+// http.HandlerFunc(handler)→Cast Index to Hander
+func BaseHandlerFunc(handler func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return BaseHandler(http.HandlerFunc(handler))
+}
+
+func BaseHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Write([]byte("MUSIC_LIST"))
+		handler.ServeHTTP(w, r)
 	})
-  http.ListenAndServe(":3000", nil)
-    
 }
